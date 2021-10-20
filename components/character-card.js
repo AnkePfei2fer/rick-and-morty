a0037cd5051651e338c5f8a8de3b1f3e974eed19
Anke <pfeiffer.anke@outlook.com>
2021-10-20 12:01:56 +0200

    ♻️ Remove div element, replace static values with array entries

--- components/character-card.js
+++ components/character-card.js
@@ -1,29 +1,38 @@
 import { createElement } from "../lib/elements";
 import styles from "./characterCard.module.css";
 
-export default function createCharacterCard({ name, location, appearance }) {
+export default function createCharacterCard({
+  imgSrc,
+  name,
+  status,
+  species,
+  lastKnownLocation,
+  firstSeenIn,
+}) {
   const characterCardElement = createElement(
     "article",
     { className: styles.card },
     [
       createElement("img", {
-        src: "./components/Bepisian.jpeg",
+        src: imgSrc,
         className: styles.cardPicture,
         alt: "",
       }),
-      createElement("div", { className: styles.cardText }, [
-        createElement("h2", { className: styles.cardHeader }, [name]),
-        createElement("h4", { className: styles.cardSubtitle }, [
-          "Alive - Alien",
-        ]),
-        createElement("span", { className: styles.cardSectionTitle }, [
-          "Last known location:",
-        ]),
-        createElement("h3", { className: styles.cardLocation }, [location]),
-        createElement("span", { className: styles.cardSectionTitle }, [
-          "First seen in:",
-        ]),
-        createElement("h3", { className: styles.cardAppearance }, [appearance]),
+      createElement("h2", { className: styles.cardHeader }, [name]),
+      createElement("span", { className: styles.cardSubtitle }, [
+        status + " - " + species,
+      ]),
+      createElement("h4", { className: styles.cardSectionTitle }, [
+        "Last known location:",
+      ]),
+      createElement("span", { className: styles.cardLocation }, [
+        lastKnownLocation,
+      ]),
+      createElement("h4", { className: styles.cardSectionTitle }, [
+        "First seen in:",
+      ]),
+      createElement("span", { className: styles.cardAppearance }, [
+        firstSeenIn,
       ]),
     ]
   );
